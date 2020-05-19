6cf3353a902d2d6b2b393b15b875d9a594b2ad11
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2020-05-19 13:12:14 +0100

    :lipstick: Add Material-UI Icons, node-sass and implement functional component for basic Navbar :lipstick:

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Navbar from "./components/scaffolding/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>ShadowSpark</h1>
+      <Navbar />
     </div>
   );
 }
