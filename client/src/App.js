a62840d0b5685d4958ce5228d39cb8b668f6b688
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2020-05-20 10:39:15 +0100

    :construction: Setup Redux Store for State Management :construction:

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import reduxStore from "./reduxStore";
 import Navbar from "./components/scaffolding/Navbar";
 import Landing from "./components/scaffolding/Landing";
 import Registration from "./components/auth/Registration";
@@ -7,14 +9,16 @@ import Login from "./components/auth/Login";
 
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <Navbar/>
-                <Route exact path="/" component={Landing} />
-                <Route exact path="/register" component={Registration} />
-                <Route exact path="/login" component={Login} />
-            </div>
-        </Router>
+        <Provider store={reduxStore}>
+            <Router>
+                <div className="App">
+                    <Navbar />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/register" component={Registration} />
+                    <Route exact path="/login" component={Login} />
+                </div>
+            </Router>
+        </Provider>
     );
 }
 
---
+++ client/src/actions/types.js
@@ -0,0 +1,3 @@
+export const GET_ERRORS = "GET_ERRORS";
+export const DEV_LOADING = "DEV_LOADING";
+export const SET_CURRENT_DEV = "SET_CURRENT_DEV";
